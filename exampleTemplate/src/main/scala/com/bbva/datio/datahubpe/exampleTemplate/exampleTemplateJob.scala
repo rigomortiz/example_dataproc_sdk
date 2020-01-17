405d397bf6d83b43488dd342e72b7536e9ce207f
Jorge Eyner Arevalo Arica <jorge.arevalo.arica.contractor@bbva.com>
2020-01-17 11:48:07 -0500

    version 0.2.2: Adecuación de example y actualización de versión

--- exampleTemplate/src/main/scala/com/bbva/datio/datahubpe/exampleTemplate/exampleTemplateJob.scala
+++ exampleTemplate/src/main/scala/com/bbva/datio/datahubpe/exampleTemplate/exampleTemplateJob.scala
@@ -3,13 +3,9 @@ package com.bbva.datio.datahubpe.exampleTemplate
 import com.bbva.datio.datahubpe.exampleTemplate.transformations.ConcreteTransformer
 import com.bbva.datio.datahubpe.utils.processing.data.{DataReader, DataWriter}
 import com.bbva.datio.datahubpe.utils.processing.flow.{FlowInitSpark, Transformer}
+import com.typesafe.config.Config
 
-
-trait exampleTemplateTrait extends FlowInitSpark {
-
-  override def getTransformer(): Transformer[DataReader, DataWriter] = new ConcreteTransformer(config)
-
-
+object exampleTemplateJob extends FlowInitSpark {
+  override def getTransformer(config: Config): Transformer[DataReader, DataWriter] =
+    new ConcreteTransformer(config)
 }
-
-object exampleTemplateJob extends exampleTemplateTrait with FlowInitSpark
