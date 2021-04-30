0ad9b3cf3915c0dd51b222a7465f38d76f442eff
Jorge Eyner Arevalo Arica <jorge.arevalo.arica.contractor@bbva.com>
2021-04-30 17:32:39 -0500

    fix has Path in ConcreteSchemaReader

--- datahub-pe-utils/src/main/scala/com/bbva/datio/datahubpe/utils/processing/flow/impl/ConcreteSchemaReader.scala
+++ datahub-pe-utils/src/main/scala/com/bbva/datio/datahubpe/utils/processing/flow/impl/ConcreteSchemaReader.scala
@@ -7,7 +7,11 @@ import org.apache.spark.sql.types.StructType
 
 class ConcreteSchemaReader extends SchemaReader {
   def read(config: Config): StructType = {
-    val schemaConfig = config.getConfig(SCHEMA_CONF_KEY)
-    readSchema(schemaConfig, includeMetadata = true).getOrElse(new StructType())
+    if (config.hasPath(SCHEMA_CONF_KEY)) {
+      val schemaConfig = config.getConfig(SCHEMA_CONF_KEY)
+      readSchema(schemaConfig, includeMetadata = true).getOrElse(new StructType())
+    } else {
+      new StructType()
+    }
   }
 }
