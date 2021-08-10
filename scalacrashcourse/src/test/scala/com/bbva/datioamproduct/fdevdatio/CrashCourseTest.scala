726bc1870f086afd8ce2f4e1ba7b660efc7a43dc
luisangel.martinez.anaya <luisangel.martinez.anaya@bbva.com>
2021-08-09 19:45:01 -0500

    [ADD] Unit test

--- scalacrashcourse/src/test/scala/com/bbva/datioamproduct/fdevdatio/CrashCourseTest.scala
+++ scalacrashcourse/src/test/scala/com/bbva/datioamproduct/fdevdatio/CrashCourseTest.scala
@@ -4,12 +4,22 @@ package com.bbva.datioamproduct.fdevdatio
 import com.datio.dataproc.sdk.api.context.RuntimeContext
 import com.typesafe.config.Config
 import org.scalatest.{BeforeAndAfterEach, FunSuite, Matchers}
+import com.datio.dataproc.sdk.launcher.process.config.ProcessConfigLoader
 
 
 class CrashCourseTest extends FunSuite with Matchers with ContextProvider with BeforeAndAfterEach {
 
   test("should test CrashCourse") {
+    // Given
+    val config = new ProcessConfigLoader().fromPath("src/test/resources/config/application-test.conf")
+    val runtimeContext = new FakeRuntimeContext(config)
+    val jobConfig = config.getConfig("CrashCourse")
 
+    // When
+    val exitCode = new CrashCourse().runProcess(runtimeContext)
+
+    // Then
+    exitCode shouldBe 0
   }
 
 
