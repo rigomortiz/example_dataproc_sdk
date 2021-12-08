7a3e7e6dfc6b374789b0bc5b1470479e8de1a403
Daniel García <danieladan.garcia.contractor@bbva.com>
2021-12-08 17:19:54 -0600

    Fix write with options and schema validation

--- datahub-pe-utils/src/main/scala/com/bbva/datio/datahubpe/utils/processing/flow/impl/ConcreteWriter.scala
+++ datahub-pe-utils/src/main/scala/com/bbva/datio/datahubpe/utils/processing/flow/impl/ConcreteWriter.scala
@@ -4,12 +4,15 @@ import com.bbva.datio.datahubpe.utils.processing.conf.SchemaConfigValidator
 import com.bbva.datio.datahubpe.utils.processing.data.DataWriter
 import com.bbva.datio.datahubpe.utils.processing.flow.Writer
 import com.datio.dataproc.kirby.api.Output
-import com.datio.dataproc.kirby.core.actions.{DynamicDatasetSchema, Encrypt, Repartition, ReprocessPartitions, SafeReprocess, Validation}
+import com.datio.dataproc.kirby.core.actions.{DynamicDatasetSchema, Encrypt, InternalKirbyAction, Repartition, ReprocessPartitions, SafeReprocess, Validation}
 import com.datio.dataproc.kirby.core.config.configurable.KirbyConfigurableFactory
 import com.datio.dataproc.kirby.core.config.exception.MissingMandatoryOutputSchemaException
 import com.typesafe.config.Config
+import org.apache.spark.sql.DataFrame
+import org.slf4j.{Logger, LoggerFactory}
 
 class ConcreteWriter(config: Config) extends Writer[DataWriter] {
+  private val logger: Logger = LoggerFactory.getLogger(this.getClass)
   private val outputFactory = new KirbyConfigurableFactory[Output]()
 
   override def write(dataWriter: DataWriter): Unit = {
@@ -20,21 +23,28 @@ class ConcreteWriter(config: Config) extends Writer[DataWriter] {
       .getKeys()
       .foreach(key => {
         val itemWriter = dataWriter.getItemWriter(key)
-        val acceptableOutputConf = schemaConfigValidator.replaceLabels(outputKeyConfigReader.path + "." + key)
+        val acceptableOutputConf: Config = schemaConfigValidator.replaceLabels(outputKeyConfigReader.path + "." + key)
 
         val safeReprocess = SafeReprocess(acceptableOutputConf)
         val schema = Some(Option(outputFactory.getImplementation(acceptableOutputConf).getSchema.orElse(None.orNull))
           .getOrElse(throw MissingMandatoryOutputSchemaException()))
 
-        val outputInternalActions = Seq(Encrypt(schema, safeReprocess, None.orNull),
+        val outputInternalActions: Seq[InternalKirbyAction] = Seq(
+          Encrypt(schema, safeReprocess, None.orNull),
           safeReprocess.prepareOutputAction(),
-          Repartition(config),
-          DynamicDatasetSchema(config, schema),
+          Repartition(acceptableOutputConf),
+          DynamicDatasetSchema(acceptableOutputConf, schema),
           Validation(schema),
-          ReprocessPartitions(config))
+          ReprocessPartitions(acceptableOutputConf)
+        )
 
-        outputFactory.getImplementation(outputInternalActions
-          .foldLeft(acceptableOutputConf)((c, a) => a.prepareConfig(c))).write(itemWriter.df)
+        logger.debug("Apply pre output internal actions [Write Options and Schema validation]")
+        val validatedOutputDf: DataFrame = outputInternalActions.foldLeft(itemWriter.df)((ds, action) => action.doAction(ds))
+
+        outputFactory.getImplementation(
+          outputInternalActions.foldLeft(acceptableOutputConf)((c, a) => a.prepareConfig(c))
+        ).write(validatedOutputDf)
+
       })
   }
 }
