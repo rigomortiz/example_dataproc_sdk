7a9107c17416537eb8a1d5e406b44773540217a0
Rigoberto Canseco <rigobertocanseco@comunidad.unam.mx>
2022-07-08 17:28:53 -0500

    Exercise 1. Select, filter, drop columns

--- readCSV/src/main/scala/com/bbva/datioamproduct/fdevdatio/ReadCsvProcess.scala
+++ readCSV/src/main/scala/com/bbva/datioamproduct/fdevdatio/ReadCsvProcess.scala
@@ -1,12 +1,14 @@
 package com.bbva.datioamproduct.fdevdatio
 
 import com.bbva.datioamproduct.fdevdatio.common.ConfigConstants.{BIKES_CONFIG, CUSTOMER_CONFIG, MESSAGE}
+import com.bbva.datioamproduct.fdevdatio.common.StaticValues.{ABR, AMERICA, COUNTRY, MESSAGE_COL, PURCHASE_CONTINENT, PURCHASE_ONLINE}
 import com.bbva.datioamproduct.fdevdatio.utils.IOUtils
 import com.datio.dataproc.sdk.api.SparkProcess
 import com.datio.dataproc.sdk.api.context.RuntimeContext
 import com.datio.dataproc.sdk.datiosparksession.DatioSparkSession
 import com.typesafe.config.Config
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{Column, DataFrame}
+import org.apache.spark.sql.functions.{col, lit, lower}
 import org.slf4j.{Logger, LoggerFactory}
 
 import scala.util.{Failure, Success, Try}
@@ -24,15 +26,37 @@ class ReadCsvProcess extends SparkProcess with IOUtils {
       logger.info(s"Message: $message")
       logger.info("Read Bikes CSV")
       val bikesConfig = config.getConfig(BIKES_CONFIG)
-      val dfBikes:DataFrame = read(bikesConfig)
-      dfBikes.printSchema()
-      dfBikes.show(false)
+      val bikesDf:DataFrame = read(bikesConfig)
+      bikesDf.printSchema()
+      bikesDf.show(false)
 
       logger.info("Read Customer CSV")
       val customerConfig = config.getConfig(CUSTOMER_CONFIG)
-      val dfCustomer:DataFrame = read(customerConfig)
-      dfCustomer.printSchema()
-      dfCustomer.show(false)
+      val customerDf:DataFrame = read(customerConfig)
+      customerDf.printSchema()
+
+      val column:Column = lit(message).alias("message")
+      val columns: Array[Column] = customerDf.columns.map(s => col(s))
+      /*
+       +: element concat array
+       :+ array concat element
+       ++ concat
+
+       Comparaciones
+       === igual que
+       =!= diferente
+
+       */
+      // val lowerCountryColumn = lower(col("country")).alias("abr")
+      customerDf
+        .select(customerDf.columns.map {
+          case name: String if name == COUNTRY => lower(col(COUNTRY)).alias(ABR)
+          case _@name => col(name)
+        } :+ lit(message).alias(MESSAGE_COL) :_*)
+        .filter(col(PURCHASE_CONTINENT) === AMERICA)
+        .drop(col(PURCHASE_ONLINE))
+        .show(false)
+
 
     } match {
       case Success(_) => 0
---
+++ readCSV/src/main/scala/com/bbva/datioamproduct/fdevdatio/common/StaticValues.scala
@@ -0,0 +1,10 @@
+package com.bbva.datioamproduct.fdevdatio.common
+
+object StaticValues {
+  val COUNTRY: String = "country"
+  val AMERICA: String = "America"
+  val PURCHASE_CONTINENT: String = "purchase_continent"
+  val PURCHASE_ONLINE: String = "purchase_online"
+  val MESSAGE_COL:String = "message"
+  val ABR: String = "abr"
+}
