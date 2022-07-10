71283705a304624782f249529461775f7e4be470
Rigoberto Canseco <rigobertocanseco@comunidad.unam.mx>
2022-07-10 16:06:24 -0500

    Exercise 3. Save in parquet files

--- readCSV/src/main/scala/com/bbva/datioamproduct/fdevdatio/ReadCsvProcess.scala
+++ readCSV/src/main/scala/com/bbva/datioamproduct/fdevdatio/ReadCsvProcess.scala
@@ -1,12 +1,13 @@
 package com.bbva.datioamproduct.fdevdatio
 
-import com.bbva.datioamproduct.fdevdatio.common.ConfigConstants.{BIKES_CONFIG, CUSTOMER_CONFIG, MESSAGE}
+import com.bbva.datioamproduct.fdevdatio.common.ConfigConstants.{BIKES_CONFIG, CUSTOMER_BIKES_CONFIG, CUSTOMER_CONFIG, MESSAGE}
 import com.bbva.datioamproduct.fdevdatio.common.namings.output.CustomerBikes.{IsHybridCustomer, IsInPlaceCustomer, IsOnlineCustomer, NBikes, TotalInPlace, TotalOnline, TotalRefund, TotalSpent}
 import com.bbva.datioamproduct.fdevdatio.transformations.Transformations.{CustomerBikes, CustomerDf}
 import com.bbva.datioamproduct.fdevdatio.utils.IOUtils
 import com.datio.dataproc.sdk.api.SparkProcess
 import com.datio.dataproc.sdk.api.context.RuntimeContext
 import com.datio.dataproc.sdk.datiosparksession.DatioSparkSession
+import com.datio.dataproc.sdk.schema.exception.DataprocSchemaException.InvalidDatasetException
 import com.typesafe.config.Config
 import org.apache.spark.sql.DataFrame
 import org.slf4j.{Logger, LoggerFactory}
@@ -17,25 +18,26 @@ class ReadCsvProcess extends SparkProcess with IOUtils {
   private val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
   override def runProcess(runtimeContext: RuntimeContext): Int = {
-    logger.info("ReadCsvProcess.runProcess")
+    logger.info(">>> ReadCsvProcess.runProcess")
 
     Try {
       lazy val datioSparkSession: DatioSparkSession = DatioSparkSession.getOrCreate()
       val config: Config = runtimeContext.getConfig
       val message: String = config.getString(MESSAGE)
-      logger.info(s"Message: $message")
-      logger.info("Read Bikes CSV")
+      logger.info(s">>> Message: $message")
+      logger.info(">>> Read Bikes CSV")
       val bikesConfig = config.getConfig(BIKES_CONFIG)
       val bikesDf:DataFrame = read(bikesConfig)
       bikesDf.printSchema()
       bikesDf.show(false)
 
-      logger.info("Read Customer CSV")
+      logger.info(">>> Read Customer CSV")
       val customerConfig = config.getConfig(CUSTOMER_CONFIG)
       val customerDf:DataFrame = read(customerConfig)
       customerDf.printSchema()
 
-      val outputCustomerDF = customerDf
+      logger.info(">>> Transforms")
+      val outputCustomerDF: DataFrame = customerDf
         //.rule(message)
         .ruleTwo()
         .joinBikes(bikesDf)
@@ -50,10 +52,20 @@ class ReadCsvProcess extends SparkProcess with IOUtils {
           TotalRefund()
         )
         .replacePurchaseCity
-        .show(false)
+
+      logger.info(">>> Save output")
+      val customerBikesConfig = config.getConfig(CUSTOMER_BIKES_CONFIG)
+      outputCustomerDF.show(false)
+      write(customerBikesConfig, outputCustomerDF)
 
     } match {
       case Success(_) => 0
+      case Failure(exception: InvalidDatasetException) => {
+        exception.getErrors.toArray.foreach {
+          err => logger.error(err.toString)
+        }
+        -1
+      }
       case Failure(ex) => {
         ex.printStackTrace()
         -1
--- readCSV/src/main/scala/com/bbva/datioamproduct/fdevdatio/utils/IOUtils.scala
+++ readCSV/src/main/scala/com/bbva/datioamproduct/fdevdatio/utils/IOUtils.scala
@@ -1,15 +1,17 @@
 package com.bbva.datioamproduct.fdevdatio.utils
 
-import com.bbva.datioamproduct.fdevdatio.common.ConfigConstants.{BIKES_CONFIG, DELIMITER, HEADERS, PATHS, SCHEMA, TYPE}
+import com.bbva.datioamproduct.fdevdatio.common.ConfigConstants._
 import com.datio.dataproc.sdk.datiosparksession.DatioSparkSession
+import com.datio.dataproc.sdk.io.output.DatioDataFrameWriter
 import com.datio.dataproc.sdk.schema.DatioSchema
 import com.typesafe.config.Config
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{DataFrame, SaveMode}
 
 import java.net.URI
 
 trait IOUtils {
   lazy val datioSparkSession: DatioSparkSession = DatioSparkSession.getOrCreate()
+
   def read(inputConfig: Config): DataFrame = {
     val path: String = inputConfig.getStringList(PATHS).get(0)
     inputConfig.getString(TYPE) match {
@@ -26,5 +28,31 @@ trait IOUtils {
       }
       case _@inputType => throw new Exception(s"Formato de archivo no soportado: $inputType")
     }
+  }
+
+  def write(outputConfig: Config, df: DataFrame): Unit = {
+    val mode: SaveMode = outputConfig.getString(MODE) match {
+      case OVERWRITE => SaveMode.Overwrite
+      case APPEND => SaveMode.Append
+      case _@saveMode => throw new Exception(s"Modo de escritura no soportado: $saveMode")
+    }
+    val path: String = outputConfig.getString(PATH)
+    val schemaPath: String = outputConfig.getString(SCHEMA)
+    val schema: DatioSchema = DatioSchema.getBuilder.fromURI(URI.create(schemaPath)).build()
+    val partitions: Array[String] = outputConfig.getList(PARTITIONS).toArray.map(_.toString)
+
+    val write: DatioDataFrameWriter = datioSparkSession
+      .write()
+      .mode(mode)
+      .datioSchema(schema)
+      .partitionBy(partitions: _*)
+
+    outputConfig.getString(TYPE) match {
+      case CSV => write.csv(df, path)
+      case AVRO => write.avro(df, path)
+      case PARQUET => write.parquet(df, path)
+      case _@outputType => throw new Exception(s"Formato no soportado: $outputType")
+    }
+
   }
 }
